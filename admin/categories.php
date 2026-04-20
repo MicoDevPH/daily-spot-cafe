a3a1ef6d5fce15e62abf04210469085278157cce
Mico Nakase <miconakaseyt@gmail.com>
2026-04-20 18:07:03 +0800

    .

--- admin/categories.php
+++ admin/categories.php
@@ -63,7 +63,7 @@
         <div class="dashboard-wrapper">
             <nav class="top-navbar">
                 <div class="d-flex align-items-center justify-content-between">
-                    <i class="bi bi-list fs-4" style="-webkit-text-stroke: 1px;"></i>
+                    <i class="bi bi-layout-sidebar-inset fs-4"></i>
                     <div class="d-flex align-items-center gap-2 top-navbar-actions">
                         <button type="button" class="btn btn-light btn-sm top-navbar-icon">
                             <i class="bi bi-bell"></i>
